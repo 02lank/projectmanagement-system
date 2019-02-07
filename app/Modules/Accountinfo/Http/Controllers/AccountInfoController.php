65b3278027fef7ef260fdea4201d1db5d8ffa39f
EJ <38564999+02lank@users.noreply.github.com>
2019-02-07 11:36:25 +0800

    Added AccountInfoController
    I added AccountInfoController yay

--- app/Modules/Accountinfo/Http/Controllers/AccountInfoController.php
+++ app/Modules/Accountinfo/Http/Controllers/AccountInfoController.php
@@ -1,10 +1,110 @@
 <?php
 
-namespace App\Modules\AccountInfoController\Http\Controllers;
+namespace App\Modules\AccountInfo\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Modules\AccountInfo\Models\AccountInfo;
+use App\Http\Controllers\Controller;
+use Damnyan\Cmn\Services\ApiResponse;
+use Illuminate\Support\Facades\Validator;
 
-class AccountInfoController extends Controller
+class AccountController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Damnyan\Cmn\Services\ApiResponse
+     */
+    public function index()
+    {
+        // GET ACCOUNTS
+        $accountinfos= AccountInfo::paginate(10);
+        return (new ApiResponse)->resource($accountinfos); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Damnyan\Cmn\Services\ApiResponse
+     */
+    public function store(Request $request)
+    {
+        // //STORE ACCOUNT
+        // $account = $request->isMethod('put') ? Account::findOrFail($request->account_id) : new Account;
+
+        // $account->user_id = $request->input('user_id');
+        // $account->username = $request->input('username');
+        // $account->password = $request->input('password');
+        
+        // if ($account->save()) {
+        //     return new AccountResource($account);
+        // }
+        $payload = $request->only('accountinfo_id', 'firstName', 'lastName');
+        $rules = [
+            'accountinfo_id'   => 'required',
+            'firstName' => 'required',
+            'lastName' => 'required'
+        ];
+        $validator = Validator::make($payload, $rules);
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json(
+                [
+                    "message" => "Unprocessable Entity",
+                    "errors" => $errors
+                ],
+                422
+            );
+        }
+        $payload = $request->all();
+        $account = Account::create($payload);
+        return (new ApiResponse)->resource($account);
+    }
+    
+    /**
+    * Display the specified resource.
+    *
+    * @param int  $id
+    *
+    * @return \Damnyan\Cmn\Services\ApiResponse
+    */
+    public function show($account_id)
+    {
+        $account = Account::findOrFail($account_id);
+        return (new ApiResponse)->resource($account);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $account_id)
+    {
+        $account = Account::findOrFail($account_id);
+        $account->update($request->only('username', 'password'));
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Damnyan\Cmn\Services\ApiResponse
+     */
+    public function destroy($account_id)
+    {
+        // // DELETE ACCOUNT
+        // $account= Account::findOrFail($account_id);
+
+        // if ($account->delete()) {
+        //     return new AccountResource($account);
+        // }
+        $account = Account::findOrFail($account_id);
+        $account->delete();
+        return (new ApiResponse)->resource($account);
+    }
 }
