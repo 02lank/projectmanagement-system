84bae6c9f3443bd1cb1270f53a5f116cb7ec36c6
Mitzie Magdadaro <mitzmagdadaro@gmail.com>
2019-02-15 14:25:34 +0800

    installed jwt

--- app/Modules/Task/Routes/task/index.php
+++ app/Modules/Task/Routes/task/index.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::group(
-    ['middleware' => []], function () {
+Route::group([
+    'middleware' => []
+    
+], function () {
         Route::get('/', 'TaskController@index');
         Route::get('/user', 'TaskController@indexUser');
         Route::post('/', 'TaskController@store');
-    }
-);
+});
