029b7a30b0492638ca95046b0ab08e4d7ea56242
Erwin Raphael Piolo Esmeria <p.esmeria11@gmail.com>
2019-02-26 09:23:31 +0800

    JWT Verification and Fix Database

--- app/Modules/Account/Routes/account/index.php
+++ app/Modules/Account/Routes/account/index.php
@@ -1,13 +1,13 @@
 <?php
 Route::group(['middleware' => ['jwt.verify']], function () {
         Route::get('/', 'AccountController@index');
-        Route::post('/', 'AccountController@store');
         Route::get('user', 'AuthController@getAuthenticatedUser');
         Route::get('closed', 'DataController@closed');
     }
 );
 
 
+Route::post('/', 'AccountController@store');
 Route::post('login', 'AccountController@authenticate');
 Route::get('open', 'DataController@open');
 
